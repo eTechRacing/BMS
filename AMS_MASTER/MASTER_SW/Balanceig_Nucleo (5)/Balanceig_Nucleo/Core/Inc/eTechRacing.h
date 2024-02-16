53cd43d8a3eb3b26c5f7a0c5cf7974394c80c236
Becato7 <bernacarnota@gmail.com>
2024-02-16 14:05:03 +0100

    Balanceo

---
+++ AMS_MASTER/MASTER_SW/Balanceig_Nucleo (5)/Balanceig_Nucleo/Core/Inc/eTechRacing.h
@@ -0,0 +1,15 @@
+/*
+ * eTechRacing.h
+ *
+ *  Created on: Feb 15, 2024
+ *      Author: Bernat
+ */
+
+#ifndef INC_ETECHRACING_H_
+#define INC_ETECHRACING_H_
+
+#include "main.h"
+
+void delay_us (uint16_t us, TIM_HandleTypeDef timer);
+
+#endif /* INC_ETECHRACING_H_ */
